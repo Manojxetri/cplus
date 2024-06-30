778fb058f3a383f82bb4092602de481c0226dcf2
Manojxetri <allinone23450@gmail.com>
2024-06-30 07:38:14 +0545

    using of operator overlaoding

--- operatoroverloading/main.cpp
+++ operatoroverloading/main.cpp
@@ -1,3 +1,75 @@
+//#include <iostream>
+
+//using namespace std;
+
+//class Distance{
+  //          private:
+    //        int inch,foot;
+      //      public:
+        //    Distance(){};
+          //  Distance(int i,int f){
+            //inch=i;
+            //foot=f;
+            //}
+            //void addDistance(Distance d1,Distance d2){
+            //foot=d1.foot+d2.foot;
+            //inch=d1.inch+d2.inch;
+            //foot=(foot+inch)/12;
+            //inch=inch%12;
+            //}
+            //void display()
+            //{
+              //cout <<"foot is" <<foot <<endl;
+              //cout <<"inch is " <<inch <<endl;
+            //}
+
+//};
+//int main()
+//{
+//
+  //  Distance d1(5,4);
+    //d1.display();
+    //return 0;
+//}
+
+
+
+/*#include <iostream>
+
+using namespace std;
+
+class Distance{
+            private:
+            int inch,foot;
+            public:
+            Distance(){};
+            Distance(int i,int f){
+            inch=i;
+            foot=f;
+            }
+            Distance addDistance(Distance d1,Distance d2){
+            foot=d1.foot+d2.foot;
+            inch=d1.inch+d2.inch;
+            foot=(foot+inch)/12;
+            inch=inch%12;
+            return Distance(foot,inch);
+            }
+            void display(){
+               cout <<"foot:" <<foot <<"inch:" <<inch <<endl;
+            }
+
+};
+int main()
+{
+
+    Distance d1(5,4),d2(10,4),d3;
+    d3.addDistance(d1,d2);
+    d3.display();
+    return 0;
+}*/
+
+//using operator overloading
+
 #include <iostream>
 
 using namespace std;
@@ -11,16 +83,25 @@ class Distance{
             inch=i;
             foot=f;
             }
-            void addDistance(Distance d1,Distance d2){
-            foot=d1.foot+d2.foot;
-            inch=d1.inch+d2.inch;
-            foot=(foot+inch)/12;
-            inch=inch%12;
+            Distance operator +(Distance d){
+            Distance dist;
+            dist.foot=foot+d.foot;
+            dist.inch=inch+d.inch;
+            dist.foot=(dist.foot+dist.inch)/12;
+            dist.inch=dist.inch%12;
+            return dist;
+            }
+            void display(){
+               cout <<"foot:" <<foot <<"inch:" <<inch <<endl;
             }
 
 };
 int main()
 {
 
+    Distance d1(5,4),d2(10,4),d3;
+    d3=d1+d2;
+    d3.display();
     return 0;
 }
+
